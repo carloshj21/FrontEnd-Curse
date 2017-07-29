cd18c9d1bbafab76eb0bd98d58716b0b9aef9be2
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-07-29 17:39:10 -0500

    Mod2Lab - To Declare a Scope Function

--- Curso 6 - AngularJS Framework Fundamentals/Module 2 - Using Controllers/Mod2Lab/app/menuController.js
+++ Curso 6 - AngularJS Framework Fundamentals/Module 2 - Using Controllers/Mod2Lab/app/menuController.js
@@ -2,5 +2,8 @@ app.controller('menuController', [
     '$scope',
     function($scope){
         $scope.model = {title: 'Our Menu'};
+        $scope.changeMainDish = function (item) {
+            $scope.model.mainDish = item;
+        }
     }
 ]);
