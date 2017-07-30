5b35951881c086d2a9f3a8b7a31f7edb10913bdc
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-07-29 22:16:06 -0500

    Mod2Lab - To Declare a Scope Watch

--- Curso 6 - AngularJS Framework Fundamentals/Module 2 - Using Controllers/Mod2Lab/app/menuController.js
+++ Curso 6 - AngularJS Framework Fundamentals/Module 2 - Using Controllers/Mod2Lab/app/menuController.js
@@ -5,5 +5,10 @@ app.controller('menuController', [
         $scope.changeMainDish = function (item) {
             $scope.model.mainDish = item;
         }
+        $scope.$watch('model.mainDish', function (newValue, oldValue) {
+            if (newValue === 'BBQ Chicken Pizza') {
+                alert('You have selected the BBQ Chicken Pizza!');
+            }
+        });
     }
 ]);
