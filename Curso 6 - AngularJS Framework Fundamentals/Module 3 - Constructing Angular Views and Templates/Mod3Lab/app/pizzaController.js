f9db5c2bf292ece6ddc6c8466f35237144f0408d
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-07-31 17:41:22 -0500

    Mod3Lab - To Build Your Input Field

--- Curso 6 - AngularJS Framework Fundamentals/Module 3 - Constructing Angular Views and Templates/Mod3Lab/app/pizzaController.js
+++ Curso 6 - AngularJS Framework Fundamentals/Module 3 - Constructing Angular Views and Templates/Mod3Lab/app/pizzaController.js
@@ -1,7 +1,9 @@
 app.controller('pizzaController', [
     '$scope',
     function ($scope) {
-        $scope.model = { title: 'Pizza Builder',
-availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'] };
+        $scope.model = {
+            title: 'Pizza Builder',
+            availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers']
+        };
     }
 ]);
