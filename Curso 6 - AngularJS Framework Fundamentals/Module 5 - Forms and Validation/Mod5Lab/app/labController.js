ac522caf866667bbc64d5b5decf93c1961a3a800
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-03 21:56:16 -0500

    Mod5Lab - To Submit the Form

--- Curso 6 - AngularJS Framework Fundamentals/Module 5 - Forms and Validation/Mod5Lab/app/labController.js
+++ Curso 6 - AngularJS Framework Fundamentals/Module 5 - Forms and Validation/Mod5Lab/app/labController.js
@@ -2,11 +2,16 @@ app.controller('labController', [
     '$scope',
     function ($scope) {
         $scope.reset = reset;
+        $scope.submit = submit;
 
         reset();
         
         function reset() {
             $scope.model = {};
+        }
+
+        function submit(model) {
+            alert('Submitted\n' + JSON.stringify(model));
         }
     }
 ]);
