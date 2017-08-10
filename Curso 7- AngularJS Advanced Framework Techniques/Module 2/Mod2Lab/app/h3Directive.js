32dd84cb88aae10dfe5c4e06946eaa42a3b4d174
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-10 17:43:08 -0500

    Mod2Lab - To Modify Document Properties

--- Curso 7- AngularJS Advanced Framework Techniques/Module 2/Mod2Lab/app/h3Directive.js
+++ Curso 7- AngularJS Advanced Framework Techniques/Module 2/Mod2Lab/app/h3Directive.js
@@ -1,10 +1,19 @@
 angular.module('app').directive('h3Directive', function () {
     return {
+        restrict: 'EA',
+        replace: true,
         scope: {
             title: '@'
         },
-        restrict: 'E',
-        template: '<h3>{{title}}</h3>'
+        template: '<h3>{{title}}</h3>',
+        link: function (scope, element, attrs) {
+            element.bind('mouseenter', function () {
+                element.css('background-color', 'red')
+            });
 
+            element.bind('mouseleave', function () {
+                element.css('background-color', 'silver')
+            });
+        }
     };
 });
