783fff34b660ed7a6b666928be2c4b8ec003801d
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-09 22:34:30 -0500

    Mod2Lab - To Implement the Controller

--- Curso 7- AngularJS Advanced Framework Techniques/Module 2/Mod2Lab/app/labController.js
+++ Curso 7- AngularJS Advanced Framework Techniques/Module 2/Mod2Lab/app/labController.js
@@ -1,6 +1,10 @@
-angular.module('app')
-.controller('LabController', [ 
-   function () { 
-      var vm = this; 
-   } 
+angular.module('app').controller('LabController', [
+    function () {
+        var vm = this;
+
+        vm.person = {
+            name: 'Samuel Clemens',
+            penName: 'Mark Twain'
+        };
+    }
 ]);
