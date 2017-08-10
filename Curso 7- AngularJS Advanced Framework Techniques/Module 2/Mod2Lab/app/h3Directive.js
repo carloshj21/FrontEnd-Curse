02654ab1a3fb3608e4b82d40f6d2b89555a51b24
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-09 22:17:49 -0500

    Mod2Lab - To Display the H3 Directive

--- Curso 7- AngularJS Advanced Framework Techniques/Module 2/Mod2Lab/app/h3Directive.js
+++ Curso 7- AngularJS Advanced Framework Techniques/Module 2/Mod2Lab/app/h3Directive.js
@@ -1,4 +1,4 @@
-angular.module('app').directive('h3MessageDirective', function () {
+angular.module('app').directive('h3Directive', function () {
     return {
         scope: {
             title: '@'
