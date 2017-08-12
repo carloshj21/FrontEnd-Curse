c74b3dda3e6cf24308eb01ac5bcc5b530b6e35ea
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-12 15:05:17 -0500

    Mod5Lab - To Configure the Express Global Route

--- Curso 7- AngularJS Advanced Framework Techniques/Module 5/Mod5Lab/app.js
+++ Curso 7- AngularJS Advanced Framework Techniques/Module 5/Mod5Lab/app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+//To Configure the Express Global Route
+app.use(function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 app.listen(2319, function () {
     console.log('Our app is listening on port 2319!');
 });
