b2ce763358ba879d99206a6f677b4a49c0c2dfff
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-12 15:13:59 -0500

    Mod5Lab - To Configure Location and Route Providers

--- Curso 7- AngularJS Advanced Framework Techniques/Module 5/Mod5Lab/public/app/app.js
+++ Curso 7- AngularJS Advanced Framework Techniques/Module 5/Mod5Lab/public/app/app.js
@@ -1,1 +1,8 @@
-angular.module('app', []);
+angular.module('app', ['ngRoute']);
+
+angular.module('app').config([
+    '$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
+]);
