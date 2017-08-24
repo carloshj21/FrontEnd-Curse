004047cc6b55b086fc8e694e58d69613ce610a76
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-23 21:59:45 -0500

    Mod3Lab - To Fix Errors jsLint

--- Curso 8 - Using Open Source Web Tooling to Improve Development Proficiency/Module 3 - Linting and Testing/Mod3Lab/lintTest.js
+++ Curso 8 - Using Open Source Web Tooling to Improve Development Proficiency/Module 3 - Linting and Testing/Mod3Lab/lintTest.js
@@ -1,10 +1,12 @@
-init();
+'use strict';
 
 function init() {
-    recordCount = 3;
-    if (recordCount == 2) {
+    var recordCount = 3;
+    if (recordCount === 2) {
         console.log("we have two");
     } else {
         console.log('not two');
     }
 }
+
+init();
