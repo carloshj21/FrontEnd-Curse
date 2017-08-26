181cb32441c77f04cde097b86548cecefb5d6ba3
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2017-08-25 22:01:31 -0500

    Mod5Lab - To Implement Grunt(Linting)

--- Curso 8 - Using Open Source Web Tooling to Improve Development Proficiency/Module 5 - Gulp and Grunt/Mod5Lab/gulpFile.js
+++ Curso 8 - Using Open Source Web Tooling to Improve Development Proficiency/Module 5 - Gulp and Grunt/Mod5Lab/gulpFile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', ['uglify']);
+gulp.task('default', ['uglify', 'lint']);
 
 gulp.task('uglify', function () {
     return gulp.src('lintTest.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist-gulp'));
+});
+
+gulp.task('lint', function() {
+    gulp.src('lintTest.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
